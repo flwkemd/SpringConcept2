b92687d28e0479d78fab79648cc3caab406f43b1
flwkemd <flwkemd@naver.com>
2017-11-29 03:26:09 +0100

    Spring annotation about Component

---
+++ src/com/ex/springdemo/Coach.java
@@ -0,0 +1,6 @@
+package com.ex.springdemo;
+
+public interface Coach {
+
+		public String getDailyWorkout();
+}
---
+++ src/com/ex/springdemo/TennisCoach.java
@@ -0,0 +1,15 @@
+package com.ex.springdemo;
+
+import org.springframework.stereotype.Component;
+
+//@Component("thatSillyCoach")
+@Component
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Practice tennis";
+	}
+
+}
